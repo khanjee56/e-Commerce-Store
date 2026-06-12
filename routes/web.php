c571560d0216d007cb1789b2bc58252986e2ea61
khanjee56 <khanjeekhanjee827@gmail.com>
2026-06-12 19:50:36 +0500

    forgot and reset password with email

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 
-
-
-Auth::routes();
+// Only forgot password + email verification (no duplicate login/register)
+Auth::routes(['login' => false, 'register' => false, 'logout' => false]);
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/register',[HomeController::class, 'register'])->name('register');
@@ -20,13 +19,12 @@ Route::post('/login', [HomeController::class, 'loginsave'])->name('login.save');
 Route::post('/logout',[HomeController::class, 'logout'])->name('logout');
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
-
 Route::middleware('auth')->group(function() {
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart/add', [CartController::class, 'add']);
     Route::delete('/cart/remove', [CartController::class, 'remove']);
     Route::get('/cart/clear', [CartController::class, 'clear']);
-     Route::get('/orders/place', [OrderController::class, 'placeOrderPage']);
+    Route::get('/orders/place', [OrderController::class, 'placeOrderPage']);
     Route::post('/orders/place', [OrderController::class, 'placeOrder']);
     Route::get('/orders', [OrderController::class, 'myOrders']);
 });
